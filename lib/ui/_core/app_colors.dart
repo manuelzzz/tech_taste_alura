71128de69b218e58b2a258d37c97ca9a79a86bc5
manuelss.23.bil <manuelss.23.bil@gmail.com>
2025-04-16 03:41:53 +0000

    style: Cor de `lightBackgroundColor`

--- lib/ui/_core/app_colors.dart
+++ lib/ui/_core/app_colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 abstract class AppColors {
   static const Color backgroundColor = Color(0xFF202123);
+  static const Color lightBackgroundColor = Color(0xFF343541);
   static const Color mainColor = Color(0xFFffa559);
 
   static const Color white = Colors.white;
