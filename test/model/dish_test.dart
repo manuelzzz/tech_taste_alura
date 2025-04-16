7c592e30293f8d15423810f4557c10c950b1daa1
manuelss.23.bil <manuelss.23.bil@gmail.com>
2025-04-16 23:45:29 +0000

    Testes unitários para a classe Dish

---
+++ test/model/dish_test.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:myapp/model/dish.dart';
+
+void main() {
+  final dishMap = {
+    'id': '1',
+    'imagePath': 'path/to/image.jpg',
+    'description': 'Dish Description',
+    'price': 100,
+  };
+
+  test('Dish Model retorna classe correta do fromMap', () {
+    final dish = Dish.fromMap(dishMap);
+
+    expect(dish.id, '1');
+    expect(dish.imagePath, 'path/to/image.jpg');
+    expect(dish.description, 'Dish Description');
+    expect(dish.price, 100);
+  });
+
+  test('Dish Model retorna map correto do toMap', () {
+    final dish = Dish.fromMap(dishMap);
+    final output = dish.toMap();
+
+    expect(output['id'], '1');
+    expect(output['imagePath'], 'path/to/image.jpg');
+    expect(output['description'], 'Dish Description');
+    expect(output['price'], 100);
+  });
+}
