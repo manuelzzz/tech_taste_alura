13996b283b485528cc26cdbef17be432e9699e52
manuelss.23.bil <manuelss.23.bil@gmail.com>
2025-04-16 23:45:21 +0000

    Adicionando o teste de unidade para cobrir a lista de dish no Restaurant

--- test/model/restaurant_test.dart
+++ test/model/restaurant_test.dart
@@ -10,6 +10,14 @@ void main() {
     'stars': 4.5,
     'distance': 500,
     'categories': ['Category 1', 'Category 2'],
+    'dishes': [
+      {
+        'id': '1',
+        'imagePath': 'path/to/dish_image.jpg',
+        'description': 'Dish Description',
+        'price': 100,
+      },
+    ],
   };
 
   test('Restaurant Model retorna classe correta do fromMap', () {
@@ -22,6 +30,11 @@ void main() {
     expect(restaurant.stars, 4.5);
     expect(restaurant.distance, 500);
     expect(restaurant.categories, ['Category 1', 'Category 2']);
+    expect(restaurant.dishes.length, 1);
+    expect(restaurant.dishes[0].id, '1');
+    expect(restaurant.dishes[0].imagePath, 'path/to/dish_image.jpg');
+    expect(restaurant.dishes[0].description, 'Dish Description');
+    expect(restaurant.dishes[0].price, 100);
   });
 
   test('Restaurant Model retorna map correto do toMap', () {
@@ -35,5 +48,10 @@ void main() {
     expect(output['stars'], 4.5);
     expect(output['distance'], 500);
     expect(output['categories'], ['Category 1', 'Category 2']);
+    expect(output['dishes'].length, 1);
+    expect(output['dishes'][0]['id'], '1');
+    expect(output['dishes'][0]['imagePath'], 'path/to/dish_image.jpg');
+    expect(output['dishes'][0]['description'], 'Dish Description');
+    expect(output['dishes'][0]['price'], 100);
   });
 }
