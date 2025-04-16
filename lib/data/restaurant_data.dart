e166b6efee6a63a4bea43710c21cb2d828877f28
manuelss.23.bil <manuelss.23.bil@gmail.com>
2025-04-16 03:41:22 +0000

    feat: Classe para recuperar os restaurantes

---
+++ lib/data/restaurant_data.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:myapp/model/restaurant.dart';
+
+class RestaurantData extends ChangeNotifier {
+  List<Restaurant> listRestaurant = [];
+
+  Future<void> getRestaurants() async {
+    String jsonString = await rootBundle.loadString('assets/data.json');
+    Map<String, dynamic> data = json.decode(jsonString);
+    List<dynamic> restaurantsData = data['restaurants'];
+
+    for (final restaurant in restaurantsData) {
+      Restaurant convertedRestaurant = Restaurant.fromMap(restaurant);
+      listRestaurant.add(convertedRestaurant);
+    }
+  }
+}
