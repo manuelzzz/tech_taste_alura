2b4cf04e919dea9884d4d3035071aae0bfc82251
manuelss.23.bil <manuelss.23.bil@gmail.com>
2025-04-17 00:17:55 +0000

    chore: Função para enviar à página de restaurant

--- lib/ui/home/widgets/restaurant_widget.dart
+++ lib/ui/home/widgets/restaurant_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/model/restaurant.dart';
+import 'package:myapp/ui/restaurants/restaurant_screen.dart';
 
 class RestaurantWidget extends StatelessWidget {
   final Restaurant restaurant;
@@ -8,27 +9,37 @@ class RestaurantWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      spacing: 12.0,
-      children: [
-        Image.asset('assets/${restaurant.imagePath}', width: 72),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              restaurant.name,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-            ),
-            Row(
-              children: List.generate(
-                restaurant.stars.toInt(),
-                (index) => Image.asset('assets/others/star.png', width: 16),
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => RestaurantScreen(restaurant: restaurant),
+          ),
+        );
+      },
+      child: Row(
+        spacing: 12.0,
+        children: [
+          Image.asset('assets/${restaurant.imagePath}', width: 72),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                restaurant.name,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
               ),
-            ),
-            Text('${restaurant.distance}km'),
-          ],
-        ),
-      ],
+              Row(
+                children: List.generate(
+                  restaurant.stars.toInt(),
+                  (index) => Image.asset('assets/others/star.png', width: 16),
+                ),
+              ),
+              Text('${restaurant.distance}km'),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
