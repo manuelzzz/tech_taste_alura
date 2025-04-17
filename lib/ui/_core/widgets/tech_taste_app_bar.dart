cbe33587a7477d1b25f180373d3ede10e53c409f
Manuel Santos Souza <manuel.souza.017@ufrn.edu.br>
2025-04-16 21:47:44 -0300

    feat: componente de AppBar

---
+++ lib/ui/_core/widgets/tech_taste_app_bar.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/ui/_core/bag_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:badges/badges.dart' as badges;
+
+class TechTasteAppBar extends StatefulWidget implements PreferredSizeWidget {
+  final String? title;
+
+  const TechTasteAppBar({super.key, this.title});
+
+  @override
+  State<TechTasteAppBar> createState() => _TechTasteAppBarState();
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class _TechTasteAppBarState extends State<TechTasteAppBar> {
+  @override
+  Widget build(BuildContext context) {
+    final BagProvider provider = Provider.of<BagProvider>(context);
+
+    return AppBar(
+      title:
+          widget.title != null
+              ? Text(
+                widget.title!,
+                style: TextStyle(fontWeight: FontWeight.w600),
+              )
+              : null,
+      centerTitle: true,
+      elevation: 0,
+      actions: [
+        badges.Badge(
+          showBadge: provider.bag.isNotEmpty,
+          position: badges.BadgePosition.bottomStart(start: 0, bottom: 0),
+          badgeContent: Text(
+            provider.bag.length.toString(),
+            style: TextStyle(fontSize: 10),
+          ),
+          child: IconButton(
+            icon: const Icon(Icons.shopping_bag),
+            onPressed: () {},
+          ),
+        ),
+      ],
+    );
+  }
+}
